eafdaf4d67f4ae47fc40e7fc56ba614806b9c08a
zezhongyan <yanzezhong@outlook.com>
2020-08-14 10:46:12 +0800

    建立了一个springmvc的基本框架

---
+++ src/main/java/Controller/testController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+    @RequestMapping("/test")
+    public String test(){
+        System.out.println("成功了!!!!!");
+        return "success";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href="${pageContext.request.contextPath}/test">点击跳转</a>
+</body>
+</html>
